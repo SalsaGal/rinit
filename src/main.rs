5c069ec7bd05d3c9d571866ddcbe43fc31b6a1e3
Salsa Gal <fielddashos@gmail.com>
2021-06-06 12:38:27 +1000

    Add default template path

--- src/main.rs
+++ src/main.rs
@@ -4,27 +4,35 @@ use std::env::args;
 
 fn main() {
 	let args: Vec<String> = args().collect();
+	let template_path_length;
 
 	if let Some(template_path) = args.get(1) {
-		if let Ok(dir) = read_dir(template_path) {
-			for file in dir {
-				if let Ok(file) = file {
-					// Replace variables
-					let mut file_content = File::open(file.path()).unwrap();
-					let mut contents = String::new();
-					file_content.read_to_string(&mut contents).unwrap();
-
-					for arg in 2..args.len() {
-						contents = contents.replace(&format!("{{${}}}", arg - 2), &args[arg]);
-					}
-
-					// Copy to new directory
-					let mut destination = File::create(&file.path().to_str().unwrap().to_owned()[template_path.len()..]).unwrap();
-					destination.write_all(contents.as_bytes()).unwrap();
-				}
-			}
+		let dir = if let Ok(dir) = read_dir(template_path) {
+			template_path_length = template_path.len();
+			dir
+		} else if let Ok(dir) = read_dir(format!("{}/.config/rinit/{}/", home::home_dir().unwrap().to_string_lossy(), template_path)) {
+			template_path_length = format!("{}/.config/rinit/{}/", home::home_dir().unwrap().to_string_lossy(), template_path).len();
+			dir
 		} else {
-			eprintln!("Unable to load `{}`", template_path);
+			panic!("Unable to load `{}`", template_path);
+		};
+
+		for file in dir {
+			if let Ok(file) = file {
+				// Replace variables
+				let mut file_content = File::open(file.path()).unwrap();
+				let mut contents = String::new();
+				file_content.read_to_string(&mut contents).unwrap();
+
+				for arg in 2..args.len() {
+					contents = contents.replace(&format!("{{${}}}", arg - 2), &args[arg]);
+				}
+
+				// Copy to new directory
+				let path = &file.path().to_str().unwrap().to_owned()[template_path_length..];
+				let mut destination = File::create(path).unwrap();
+				destination.write_all(contents.as_bytes()).unwrap();
+			}
 		}
 	} else {
 		eprintln!("Enter a template to use as an argument.");
