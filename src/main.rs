81d8fa080ce564b1c65d5d4b7d95e3102a6a92c3
Salsa Gal <fielddashos@gmail.com>
2021-06-06 11:59:49 +1000

    Add argument replacements

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::fs::{copy, read_dir};
+use std::fs::{File, read_dir};
+use std::io::{Read, Write};
 use std::env::args;
 
 fn main() {
@@ -8,8 +9,18 @@ fn main() {
 		if let Ok(dir) = read_dir(template_path) {
 			for file in dir {
 				if let Ok(file) = file {
-					let destination = file.path().to_str().unwrap().to_owned();
-					copy(file.path(), &destination[template_path.len()..]).unwrap();
+					// Replace variables
+					let mut file_content = File::open(file.path()).unwrap();
+					let mut contents = String::new();
+					file_content.read_to_string(&mut contents).unwrap();
+
+					for arg in 2..args.len() {
+						contents = contents.replace(&format!("{{${}}}", arg - 2), &args[arg]);
+					}
+
+					// Copy to new directory
+					let mut destination = File::create(&file.path().to_str().unwrap().to_owned()[template_path.len()..]).unwrap();
+					destination.write_all(contents.as_bytes()).unwrap();
 				}
 			}
 		} else {
